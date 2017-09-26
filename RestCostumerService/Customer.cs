2ac5840e7d5f97ae08763454a819bf80b9fb1250
Ditte <Ditte@DESKTOP-QORFADN>
2017-09-26 13:45:42 +0200

    Påbegyndelse af opgave Restful WCF-services, igang med assignment 3

---
+++ RestCostumerService/Customer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RestCostumerService
+{
+    public class Customer
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Year { get; set; }
+
+        public Customer()
+        {
+            
+        }
+
+        public Customer(int id, string first, string last, int year)
+        {
+            ID = id;
+            FirstName = first;
+            LastName = last;
+            Year = year;
+        }
+
+
+    }
+}
--- RestCostumerService/Service1.svc.cs
+++ RestCostumerService/Service1.svc.cs
@@ -8,26 +8,51 @@ using System.Text;
 
 namespace RestCostumerService
 {
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
-    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
-    public class Service1 : IService1
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "CustomerService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select CustomerService.svc or CustomerService.svc.cs at the Solution Explorer and start debugging.
+    public class CustomerService : ICustomerService
     {
-        public string GetData(int value)
+        private static List<Customer> cList = new List<Customer>();
+
+        public IList<Customer> GetCustomers()
         {
-            return string.Format("You entered: {0}", value);
+            return cList;
         }
 
-        public CompositeType GetDataUsingDataContract(CompositeType composite)
-        {
-            if (composite == null)
-            {
-                throw new ArgumentNullException("composite");
-            }
-            if (composite.BoolValue)
-            {
-                composite.StringValue += "Suffix";
-            }
-            return composite;
-        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //public string GetData(int value)
+        //{
+        //    return string.Format("You entered: {0}", value);
+        //}
+
+        //public CompositeType GetDataUsingDataContract(CompositeType composite)
+        //{
+        //    if (composite == null)
+        //    {
+        //        throw new ArgumentNullException("composite");
+        //    }
+        //    if (composite.BoolValue)
+        //    {
+        //        composite.StringValue += "Suffix";
+        //    }
+        //    return composite;
+        //}
+
     }
 }
